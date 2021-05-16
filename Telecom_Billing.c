35483b0b5536eec13a3c53eb0277905b0841aa6e
MD Faizal <faizalmohammad67@gmail.com>
2021-05-16 20:32:31 +0530

    update user menu

--- Telecom_Billing.c
+++ Telecom_Billing.c
@@ -4,12 +4,16 @@
 #include <stdlib.h>
 char username[][20]={"faizal","kushagra","salman","nikita","pragya"};
 char passwords[][20]={"123","123","123","123","123"};
+char subs[][20]={"No subscription","Netflix","Disney+ Hotstar","Amazon prime video","ZEE5","SonyLIV"};
+
 char name[20],password[20];
 char admin_name[]={"xyz"};
 char admin_pass[]={"123"};
 long long  phone_no[]={9899483922,8887122744,7291049064,3594695879,4387643986};
 float data_used[]={39,40,20,33,10};
 float call_time[]={23,24,45,64,23};
+int user_sub[]={1,4,0,1,0}; // user subscription 
+int user_tune[]={0,1,1,0,0};//caller tune
 int id;
 void clrscr(void);
 int user_login();
@@ -17,14 +21,13 @@ int admin_login();
 int admin_menu();
 int user_menu();
 
-//i am changing the file
 int main()
 { 
   void clrscr();
   int choice;
   printf("***************************************************************\n");
   printf("--------------------------WELCOME TO THE TELECOME BILLING SYSTEM----------------------------\n");
-  printf("                          ******************************************************************\n\n");
+  printf("                         ******************************************************************\n\n");
   a:
   {
     printf("\t\t\t LOGIN WINDOW\n");
@@ -41,27 +44,93 @@ int main()
         goto a;
       }
   }
-  printf("end of the program");
+  printf("end of the program ");
 }
 
 
 int admin_menu()
-{
+{ 
    printf("this is a admin menu\n");
 }
 
 
 int user_menu()
  {
-  int choice;
+  int choice,choice_2;
+  float up_data,up_call;
+  
+  printf("----------------------------------------------------------------------------------------------------------------------------------------\n\n");
   printf("*********************MAIN WINDOW********************\n");
   printf(" * Welcome Back %s \n",username[id]);
   printf(" * %lld\n",phone_no[id]);
-  printf(" * Data consume(gb) :%f \n",data_used[id]);
-  printf(" * Call time(min) :%f\n",call_time[id]);
-  printf(" *  update data consume       update call time       \n");
- 
- } 
+  printf(" * Data consume(gb) :%.2f \n",data_used[id]);
+  printf(" * Call time(min) :%.2f\n",call_time[id]);
+  printf(" * Callertune :");
+  {
+    if(user_tune[id]==1)
+      printf("Active\n");
+    else 
+      printf("Inactive\n");  
+  }
+  printf(" * Active subscription :%s\n\n",subs[user_sub[id]]);
+  printf("----------------------------------------------------------------------------------------------------------------------------------------\n");
+  printf("  Update data consume     update call time     Activate/Deactivate caller tune      Change OTT subscription      Generate Bill\n");
+  printf("       Press 1                Press 2                     Press 3                           Press 4                  press 5  \n ");
+  printf("----------------------------------------------------------------------------------------------------------------------------------------\n");
+  printf("Enter the choice :"); 
+  scanf("%d",&choice);
+  switch(choice)
+  {
+  case 1:
+  
+    printf("Your current data consumption is :%.2f \n",data_used[id]);
+    printf("How much you want to add data value : ");
+    scanf("%f",&up_data);
+    data_used[id]+=up_data;
+    printf(" successfully data updated \n\n");
+
+    while(1)
+    {
+      printf("Press zero to back :");
+      scanf("%d",&choice_2);
+      if (choice_2==0)
+      {
+        user_menu();
+        break;
+      }  
+      else 
+      printf("Please Enter correct value\n");
+    }choice_2=1;
+  break;
+  
+  case 2:
+    printf("Your current  call time is :%.2f \n",call_time[id]);
+    printf("how much you want to add call time : \n");
+    scanf("%f",&up_call);
+    call_time[id]+=up_call;
+    printf(" successfully call time  updated \n");
+
+    while(1)
+    {
+      printf("Press zero to back :");
+      scanf("%d",&choice_2);
+      if (choice_2==0)
+      {
+        user_menu();
+        break;
+      }  
+      else 
+      printf("Please Enter correct value\n");
+    }choice_2=1; 
+  break;
+
+  default: 
+  printf("Enter the correct choice");
+    break;
+  }
+
+ }
+
 
 int admin_login()
 {
@@ -114,7 +183,6 @@ int user_login()
             if(strcmp(password, passwords[i])==0)
               {
                 id=i;
-                printf("Welcome back %s\n",username[id]);
                 user_menu();
                 break;
               }
