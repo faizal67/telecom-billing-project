e5e0853cdb56b3531020d973f2e9e09e374bc012
MD Faizal <faizalmohammad67@gmail.com>
2021-05-29 20:47:22 +0530

    add billing function

--- Telecom_Billing.c
+++ Telecom_Billing.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <string.h>
 #include <stdlib.h>
+
 char username[][20]={"faizal","kushagra","salman","nikita","pragya"};
 char passwords[][20]={"123","123","123","123","123"};
 char subs[][20]={"No subscription","Netflix","Disney+ Hotstar","Amazon prime video","ZEE5","SonyLIV"};
@@ -9,21 +10,23 @@ char subs[][20]={"No subscription","Netflix","Disney+ Hotstar","Amazon prime vid
 char name[20],password[20];
 char admin_name[]={"xyz"};
 char admin_pass[]={"123"};
-long long  phone_no[]={9899483922,8887122744,7291049064,3594695879,4387643986};
+long long int phone_no[10]={9899483922,8887122744,7291049064,3594695879,4387643986};
 float data_used[]={39,40,20,33,10};
 float call_time[]={23,24,45,64,23};
 int user_sub[]={1,4,0,1,0}; // user subscription 
 int user_tune[]={0,1,1,0,0};//caller tune
 int id;
-void clrscr(void);
+int discount=0;
 int user_login();
 int admin_login();
 int admin_menu();
 int user_menu();
+void generate_bill(int);
 
 int main()
 { 
-  void clrscr();
+  printf("\e[1;1H\e[2J");
+  system("Color D");
   int choice;
   printf("***************************************************************\n");
   printf("--------------------------WELCOME TO THE TELECOME BILLING SYSTEM----------------------------\n");
@@ -40,11 +43,62 @@ int main()
         break;
         case 2:admin_login();
         break;
-        default: printf("Wrong input enter correct input \n");
+        default: system("Color 4");printf("Wrong input enter correct input \n");
         goto a;
       }
   }
   printf("end of the program ");
+}
+
+void generate_bill(int id)
+{ 
+  printf("\e[1;1H\e[2J");
+  system("Color A");
+  float b_total,a_total,ott_charge;
+  if(user_sub[id]==1)
+  ott_charge=499;
+  else if(user_sub[id]==2)
+  {
+    ott_charge=299;
+  }
+  else if(user_sub[id]==3)
+  {
+    ott_charge=129;
+  }
+  else if(user_sub[id]==4)
+  {
+    ott_charge=355;
+  }
+  else if(user_sub[id]==5)
+  {
+    ott_charge=180;
+  }
+  else
+  ott_charge=0;
+  b_total=(data_used[id]*16)+(call_time[id]*1.2)+(user_tune[id]*78)+(ott_charge);
+  printf("\n");
+  printf("--------------------------------------------Telecome Bill---------------------------------------------------\n");
+  printf("     Name : %-20s                                                                                \n",username[id]);
+  printf("     Mobile no: %-20lld                                                                            \n",phone_no[id]);
+  printf("------------------------------------------------------------------------------------------------------------\n");
+  printf("     * Data charges:                                                                      Rs %0.2f               \n",data_used[id]*16);
+  printf("     * Call charges:                                                                      Rs %0.2f \n\n",(call_time[id]*1.2));
+  printf("     * Subscriptions:-\n");
+  printf("     --> Caller tune:                                                                     Rs %d\n",user_tune[id]*78);
+  printf("     --> Add on charges(OTT):                                                             Rs %0.2f\n\n",ott_charge); 
+  printf("     * Additional Charges:-\n");
+  printf("     --> One time charges:                                                                Rs 0.00\n");
+  printf("     --> International Roaming charges:                                                   Rs 0.00\n\n");
+  printf("     * Texable Values\n");
+  printf("     -->Central GST @18% :                                                                  Rs %-0.2f\n",(b_total/100)*18);
+  printf("------------------------------------------------------------------------------------------------------------\n");
+  printf("     * Total Current amount :                                                             Rs %0.2f\n",b_total+((b_total/100)*18));
+  printf("     * Discount :                                                                         Rs %d\n\n",discount);
+  printf("     * Total Payable amount :                                                             Rs %0.2f\n",(b_total+((b_total/100)*18))-discount);
+  printf("------------------------------------------------------------------------------------------------------------\n");
+ 
+
+
 }
 
 
@@ -56,15 +110,16 @@ int admin_menu()
 
 int user_menu()
  {
+  printf("\e[1;1H\e[2J");
   int choice,choice_2;
   float up_data,up_call;
-  
-  printf("----------------------------------------------------------------------------------------------------------------------------------------\n\n");
-  printf("*********************MAIN WINDOW********************\n");
+  system("Color 3");
+  printf("*********************MAIN WINDOW********************                                                         logout(6)        exit(0)    \n");
   printf(" * Welcome Back %s \n",username[id]);
   printf(" * %lld\n",phone_no[id]);
   printf(" * Data consume(gb) :%.2f \n",data_used[id]);
   printf(" * Call time(min) :%.2f\n",call_time[id]);
+
   printf(" * Callertune :");
   {
     if(user_tune[id]==1)
@@ -76,13 +131,14 @@ int user_menu()
   printf("----------------------------------------------------------------------------------------------------------------------------------------\n");
   printf("  Update data consume     update call time     Activate/Deactivate caller tune      Change OTT subscription      Generate Bill\n");
   printf("       Press 1                Press 2                     Press 3                           Press 4                  press 5  \n ");
-  printf("----------------------------------------------------------------------------------------------------------------------------------------\n");
+  printf("----------------------------------------------------------------------------------------------------------------------------------------\n"); 
   printf("Enter the choice :"); 
+  a:
   scanf("%d",&choice);
   switch(choice)
   {
   case 1:
-  
+    system("Color A");
     printf("Your current data consumption is :%.2f \n",data_used[id]);
     printf("How much you want to add data value : ");
     scanf("%f",&up_data);
@@ -104,8 +160,9 @@ int user_menu()
   break;
   
   case 2:
+    system("Color C");
     printf("Your current  call time is :%.2f \n",call_time[id]);
-    printf("how much you want to add call time : \n");
+    printf("how much you want to add call time : ");
     scanf("%f",&up_call);
     call_time[id]+=up_call;
     printf(" successfully call time  updated \n");
@@ -189,8 +246,16 @@ int user_menu()
     }choice_2=1; 
   break;
 
+  case 5:
+   generate_bill(id);
+  break;
+
+  case 6:
+   exit(0);
+
   default: 
   printf("Enter the correct choice");
+  goto a;
     break;
   }
 
